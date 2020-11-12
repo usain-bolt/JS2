de113f4d7cb0508c302b29101bda126179757096
usain-bolt <gubaydullin-vildan@mail.ru>
2020-11-12 07:11:51 +0300

    first homework

---
+++ lesson1_hw/server.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    let body = ""
+
+    try{
+        switch (req.url){
+            case "/": 
+                body = fs.readFileSync('./public/index.html')
+                break
+            case "/script.js": 
+                body = fs.readFileSync('./public/script.js')
+                break
+            case '/styles.css': 
+                body = fs.readFileSync('./public/styles.css')
+                break
+        }
+        res.end(body)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+const port = process.env.port ||  3000
+server.listen(port)
+
+console.log('server is started')
